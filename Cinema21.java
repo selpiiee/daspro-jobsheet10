4849ea7af9dc40a2c0ac2eb9a48987c370108376
“selpiiee” <selvynurkhoffifa@gmail.com>
2025-11-21 18:36:52 +0700

    Sel - Cinema 21 - 4

--- Cinema21.java
+++ Cinema21.java
@@ -18,8 +18,8 @@ public class Cinema21 {
         System.out.printf("%s \t %s \n", audience[3][0], audience[3][1]);
 
         System.out.println(audience.length);
-        for (int i = 0; i < audience.length; i++) {
-            System.out.println("length of row " + (i + 1) + ": " + audience[i].length);
+        for (String[] rowAudience : audience) {
+            System.out.println("Length of row " + rowAudience.length);
         }
 
     }
