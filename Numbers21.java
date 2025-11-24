a8d8a1f5be610858a9d98cf3c1726debd5897bb8
“selpiiee” <selvynurkhoffifa@gmail.com>
2025-11-24 22:21:25 +0700

    Sel - Numbers 21 - 2

--- Numbers21.java
+++ Numbers21.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 public class Numbers21 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
@@ -5,5 +8,12 @@ public class Numbers21 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
         
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Length of row " + (i + 1) + ": " + myNumbers[i].length);
+        }
     }
 }
